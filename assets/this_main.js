49f644cd2e024d9b8043960109e4b48024664705
Zhang, Feng <jumphone@outlook.com>
2025-06-01 09:32:56 +0800

    Update this_main.js

--- assets/this_main.js
+++ assets/this_main.js
@@ -6,17 +6,33 @@ function checkPassword() {
     const message = document.getElementById("message");
 
     if (input === correctPassword) {
-      document.getElementById("password-form").style.display = "none";
-      document.getElementById("protected-content").style.display = "block";
+      // 保存验证状态
+      localStorage.setItem("authenticated", "true");
+
+      showContent();
     } else {
       message.textContent = "Wrong!";
     }
   }
 
+function showContent() {
+    document.getElementById("password-form").style.display = "none";
+    document.getElementById("protected-content").style.display = "block";
+  }
 
-document.getElementById("password").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      checkPassword();
+  // 页面加载时自动检查验证状态
+window.onload = function () {
+    if (localStorage.getItem("authenticated") === "true") {
+      showContent();
     }
+  }
+
+  // 监听回车
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("password").addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        checkPassword();
+      }
+    });
   });
 
