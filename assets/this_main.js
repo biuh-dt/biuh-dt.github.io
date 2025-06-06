9d04b98bf8815f31861f96678229c638ba69ae16
Zhang, Feng <jumphone@outlook.com>
2025-06-06 16:17:37 +0800

    Update this_main.js

--- assets/this_main.js
+++ assets/this_main.js
@@ -38,3 +38,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+
+async function hashText(text) {
+    
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    return hashHex
+  }
+
+
+async function getHash(text) {
+  const hash = await hashText(text);
+  console.log(hash);
+}
